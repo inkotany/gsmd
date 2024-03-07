1237a9471e9db93a63b22a00f84978958f830554
inkotany <madeinterry@gmx.us>
2024-03-07 00:01:02 -0800

    Algos

--- backend/algo/generateId.js
+++ backend/algo/generateId.js
@@ -1,3 +1,11 @@
+const { v4: uuid, validate: uuidValidate } = require('uuid');
+
 function generateId() {
-    return 3
+    return uuid();
 }
+
+function verifyId(id) {
+    return uuidValidate(id);
+}
+
+module.exports = { generateId, verifyId };

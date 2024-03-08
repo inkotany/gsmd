80b2f44225c2cbb7a08046cb9068e87270be8ea5
inkotany <madeinterry@gmx.us>
2024-03-07 17:20:09 -0800

    Book route testing

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-    res.send('Mater Dei');
-});
+app.use(express.json());
+
+const libraryRoute = require('./routes/books');
+
+app.use('/api/library/books', libraryRoute);
 
 app.listen(PORT, () => {
     console.log(`App running at http://localhost:${PORT}`);

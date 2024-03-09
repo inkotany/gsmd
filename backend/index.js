cef5441fa6bc2d2ceb4151ca25b9e20ebe77d706
inkotany <madeinterry@gmx.us>
2024-03-08 18:52:11 -0800

    Add student route

--- backend/index.js
+++ backend/index.js
@@ -5,8 +5,10 @@ const PORT = process.env.PORT || 4000;
 app.use(express.json());
 
 const libraryRoute = require('./routes/books');
+const studentRoute = require('./routes/students');
 
 app.use('/api/library/books', libraryRoute);
+app.use('/api/students', studentRoute);
 
 app.listen(PORT, () => {
     console.log(`App running at http://localhost:${PORT}`);
